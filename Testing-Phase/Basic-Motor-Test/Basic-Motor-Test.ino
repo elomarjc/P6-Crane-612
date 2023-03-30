4c22eaadf3c19d0dc0125880bb1cf3a42e0d5fd2
jacob <elomarjc@gmail.com>
2023-03-30 11:11:29 +0200

    Updated the directory for pinDefinitions

---
+++ Testing-Phase/Basic-Motor-Test/Basic-Motor-Test.ino
@@ -0,0 +1,69 @@
+// Include libraries
+#include <Arduino.h>
+#include "/Users/jacobel-omar/Documents/P6-Crane-612/Testing-Phase/pinDefinitions.h"
+
+// The pinDefinitions needs to be set to your directory.
+
+void setup()
+{
+  // Initialize serial
+  // Serial3.begin(9600);  // Communication with head
+  Serial.begin(115200); // Communication with PC
+
+  Serial.println("--- Starting Gantry Crane ---");
+
+  // // Set input pinMode
+  // pinMode(pin_pos_x, INPUT);
+  // pinMode(pin_pos_y, INPUT);
+
+  // Set output pinMode
+  pinMode(pin_enable_x, OUTPUT);
+  pinMode(pin_enable_y, OUTPUT);
+  pinMode(pin_pwm_x, OUTPUT);
+  pinMode(pin_pwm_y, OUTPUT);
+
+  // digitalWrite(pin_enable_x, HIGH);
+  // digitalWrite(pin_enable_y, HIGH);
+}
+
+// Place trolley in center
+void loop()
+{
+  //// X-axis motor
+  Serial.println("Starting X-AXIS TRIAL!");
+  digitalWrite(pin_enable_x, HIGH);
+  if (digitalRead(pin_enable_x) == HIGH)
+  { // Is motor on?
+    for (int i = 1; i <= 3; i++)
+    {                                      // Loop 3 times
+      analogWrite(pin_pwm_x, (0.1 * 255)); // Move left, 10% duty cycle
+      delay(1000);
+      analogWrite(pin_pwm_x, (0.9 * 255)); // Move right, 90% duty cycle
+      delay(1050);
+      Serial.print("Loop: ");
+      Serial.println(i);
+    }
+  }
+  digitalWrite(pin_enable_x, LOW); // Turn motor off
+  Serial.println("DONE WITH X-AXIS TRIAL!");
+  delay(5000);
+
+  //// Y-axis motor
+  Serial.println("Starting Y-AXIS TRIAL!");
+  digitalWrite(pin_enable_y, HIGH);
+  if (digitalRead(pin_enable_y) == HIGH)
+  { // Is motor on?
+    for (int i = 1; i <= 3; i++)
+    {                                      // Loop 3 times
+      analogWrite(pin_pwm_y, (0.9 * 255)); // Move DOWN, 90% duty cycle
+      delay(1000);
+      analogWrite(pin_pwm_y, (0.1 * 255)); // Move UP, 10% duty cycle
+      delay(1400);
+      Serial.print("Loop: ");
+      Serial.println(i);
+    }
+  }
+  digitalWrite(pin_enable_y, LOW); // Turn motor off
+  Serial.println("DONE WITH Y-AXIS TRIAL!");
+  delay(4000);
+}
