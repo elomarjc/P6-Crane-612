1e4cffedc0ad8ba9cf12d3e8c6b6bb0476b41c3c
jacob <elomarjc@gmail.com>
2023-05-08 14:09:39 +0200

    angle data is correct but making the angle and the trolley work at the same time has not been acheived

--- PID-BACKUP/main controller/src/mainNEW.cpp
+++ PID-BACKUP/main controller/src/mainNEW.cpp
@@ -16,28 +16,39 @@
 #include "displayHandler.h"
 #include <PID_v1.h>
 
-
 // Define Variables we'll be connecting to
 double Setpoint_y, Input_y, Output_y, 
        Setpoint_x, Input_x, Output_x,
        Setpoint_theta, Input_theta, Output_theta;
 
 // Specify the links and initial tuning parameters
-double Kp_y = 9.68, Ki_y = 0, Kd_y = 0, 
-       Kp_x = 9.68, Ki_x = 0, Kd_x = 0,
-       Kp_theta = 1, Ki_theta = 0, Kd_theta = 0;
+double Kp_y = 9.68,
+       Ki_y = 0,     
+       Kd_y = 0,     
+       kg_y = 1,
 
-PID_v1 yPID(&Input_y, &Output_y, &Setpoint_y, Kp_y, Ki_y, Kd_y, DIRECT);
-PID_v1 xPID(&Input_x, &Output_x, &Setpoint_x, Kp_x, Ki_x, Kd_x, DIRECT);
-PID_v1 thetaPID(&Input_theta, &Output_theta, &Setpoint_theta, Kp_theta, Ki_theta, Kd_theta, DIRECT);
+       Kp_x = 9.68,
+       Ki_x = 0,
+       Kd_x = 0,
+       kg_x = 1,
+       
+       Kp_theta = 1,
+       Ki_theta = 0, 
+       Kd_theta = 0, 
+       kg_theta = 1;
+
+PID_v1 yPID(&Input_y, &Output_y, &Setpoint_y, Kp_y*kg_y, Ki_y*kg_y, Kd_y*kg_y, DIRECT);
+PID_v1 xPID(&Input_x, &Output_x, &Setpoint_x, Kp_x*kg_x, Ki_x*kg_x, Kd_x*kg_x, DIRECT);
+PID_v1 thetaPID(&Input_theta, &Output_theta, &Setpoint_theta, Kp_theta*kg_theta, Ki_theta*kg_theta, Kd_theta*kg_theta, DIRECT);
 
 float angleOffset = 0;
 int step = 0;
 
+
 float getAngleFromHead()
 {
   float angle;
-  if (Serial3.available() && Serial3.read() == '\n')
+  if (Serial3.available() > 0)
   {
     String angleData = Serial3.readStringUntil('\n');
     angle = angleData.toFloat() - angleOffset;
@@ -54,7 +65,7 @@ void angleCorrection()
   int i = 0;
   while (i < n)
   {
-    if (Serial3.available() && Serial3.read() == '\n')
+    if (Serial3.available()>0)
     {
       String angleData = Serial3.readStringUntil('\n');
       angleSum += angleData.toFloat();
@@ -72,14 +83,22 @@ void readInput() {
     Input_y = analogRead(pin_pos_y); // 1.3-(0.0015*analogRead(pin_pos_y)-0.17)
     Input_x = analogRead(pin_pos_x);
     
-    Input_theta = getAngleFromHead();
+    Input_theta = getAngleFromHead()*(-1);
     
-    // if(Input_theta>90 || Input_theta<-90){         //Sanity check angle data
-    //    while(true){
-    //        digitalWrite(pin_enable_x, LOW);        //Stop motors!
-    //        digitalWrite(pin_enable_y,LOW);
-    //        Serial.println("//Insane angle data");
-    //    }
+    //Sanity check angle data
+    while(Input_theta > 90 || Input_theta < -90) 
+    {
+    digitalWrite(pin_enable_x, LOW);        //Stop motors!
+    digitalWrite(pin_enable_y,LOW);
+    Serial.println("//Insane angle data");
+    Input_theta = getAngleFromHead()*(-1);
+    }
+
+    // //Sanity check angle data
+    // while(abs(Input_theta) < 1) 
+    // {
+    // Input_theta = 0; 
+    // Input_theta = getAngleFromHead()*(-1);
     // }
 
 }
@@ -102,13 +121,13 @@ void setup()
     Input_y = analogRead(pin_pos_y);
     Input_x = analogRead(pin_pos_x);
 
-    Setpoint_y = 100;
-    Setpoint_x = 400;
+    Setpoint_y = 400;
+    Setpoint_x = 600;
     Setpoint_theta = 100;
     
     yPID.SetSampleTime(10);
     xPID.SetSampleTime(10);
-    thetaPID.SetSampleTime(10);
+    thetaPID.SetSampleTime(1);
     
     xPID.SetOutputLimits(255*0.1, 255*0.9); // Standard PWM Range
     yPID.SetOutputLimits(255*0.1, 255*0.9); // Standard PWM Range
@@ -118,7 +137,9 @@ void setup()
     yPID.SetMode(AUTOMATIC);
     xPID.SetMode(AUTOMATIC);
     thetaPID.SetMode(AUTOMATIC);
-    angleCorrection();
+    //angleCorrection();
+    digitalWrite(pin_enable_x, HIGH);
+    Serial.println("high setup");
 }
 
 void loop()
@@ -127,39 +148,24 @@ void loop()
     yPID.Compute();
     xPID.Compute();
     thetaPID.Compute();
-    
-    // if (Setpoint_x == 400) {
-    //     step = 1;
-    // }
 
-    // if (step == 1){
-    //     delay(1000);
-    //     Serial.println("step 1 PASSED");
-    //     Setpoint_x = 650;
-    // }
-
-    // if (Input_x == 650) {
-    //     step = 0;
-    // }
-
-
+    Serial.println("LOOPING");
     digitalWrite(pin_enable_y, HIGH);
     digitalWrite(pin_enable_x, HIGH);
     analogWrite(pin_pwm_y, Output_y);
     analogWrite(pin_pwm_x, Output_x);
-    //analogWrite(pin_pwm_x, Output_theta);    // the angle is broken
 
+    int range = 25;
 
-
-    // // Limit the PWM value to the valid range
-    // if (pwm_x > 255*0.9) {
-    //     pwm_x = 255*0.9;
-    // } else if (pwm_x < 255*0.1) {
-    //     pwm_x = 255*0.1;
-    // }
+    if(Setpoint_x-range <= Input_x && Input_x <= Setpoint_x+range) {
+      analogWrite(pin_pwm_x, Output_theta);    // angle 
+      Serial.println("IF ONLY ANGLE WORKED");
+    }
+    
 
     //Serial.println("PID Input_y: " + String(Input_y) + " PID Set point: " + String(Setpoint_y) + " PID Output_y: " + String(Output_y));
-    Serial.println("PID Input_x: " + String(Input_x) + " PID Setpoint_x: " + String(Setpoint_x) + " PID Output_x: " + String(Output_x) + " Angle: " + String(Input_theta));
+    Serial.println("PID Input_x: " + String(Input_x) + ", PID Setpoint_x: " + String(Setpoint_x) + ", PID Output_x: " + String(Output_x) + ", Angle: " + String(Input_theta) + ", Kp_y: " + String(Kp_y));
+    
     // Serial.println(Output_y);
 }
 
